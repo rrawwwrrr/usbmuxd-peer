0c83240b7fb5dc82572485b60a622fe2cbb9b1ef
rrr <ruslan.ramazanov@antara-tech.ru>
2025-09-04 20:11:57 +0400

    wda & mjpeg port forward

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 export USBMUXD_SOCKET_ADDRESS=/var/run/usbmuxd
 
-usbmuxd&
-sleep 2
-# shellcheck disable=SC2164
-cd /app
+cd /app || exit
+
+# Запускаем usbmuxd в фоне, чтобы иметь возможность запускать peer
+usbmuxd -f &
+UMUXD_PID=$!
+
+# Запускаем основной процесс
 ./peer
+
+# При выходе контейнера убиваем usbmuxd
+kill $UMUXD_PID
