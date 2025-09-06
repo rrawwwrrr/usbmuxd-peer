cedfd45d93107081e622b1c0221034cb3aec4838
rrr <ruslan.ramazanov@antara-tech.ru>
2025-09-06 17:18:33 +0400

    wda & mjpeg port forward

--- goios-peer/api/websocket.go
+++ goios-peer/api/websocket.go
@@ -14,79 +14,6 @@ type State struct {
 	Wda    WdaStatus              `json:"wda"`
 }
 
-type DeviceInfo struct {
-	ActivationState                       string            `json:"ActivationState"`
-	ActivationStateAcknowledged           string            `json:"ActivationStateAcknowledged"`
-	BasebandActivationTicketVersion       string            `json:"BasebandActivationTicketVersion"`
-	BasebandCertId                        string            `json:"BasebandCertId"`
-	BasebandChipID                        string            `json:"BasebandChipID"`
-	BasebandKeyHashInformation            map[string]string `json:"BasebandKeyHashInformation"`
-	BasebandMasterKeyHash                 string            `json:"BasebandMasterKeyHash"`
-	BasebandRegionSKU                     string            `json:"BasebandRegionSKU"`
-	BasebandSerialNumber                  string            `json:"BasebandSerialNumber"`
-	BasebandStatus                        string            `json:"BasebandStatus"`
-	BasebandVersion                       string            `json:"BasebandVersion"`
-	BluetoothAddress                      string            `json:"BluetoothAddress"`
-	BoardId                               string            `json:"BoardId"`
-	BootSessionID                         string            `json:"BootSessionID"`
-	BrickState                            string            `json:"BrickState"`
-	BuildVersion                          string            `json:"BuildVersion"`
-	CPUArchitecture                       string            `json:"CPUArchitecture"`
-	CarrierBundleInfoArray                []interface{}     `json:"CarrierBundleInfoArray"`
-	CertID                                string            `json:"CertID"`
-	ChipID                                string            `json:"ChipID"`
-	ChipSerialNo                          string            `json:"ChipSerialNo"`
-	DeviceClass                           string            `json:"DeviceClass"`
-	DeviceColor                           string            `json:"DeviceColor"`
-	DeviceName                            string            `json:"DeviceName"`
-	DieID                                 string            `json:"DieID"`
-	EthernetAddress                       string            `json:"EthernetAddress"`
-	FirmwareVersion                       string            `json:"FirmwareVersion"`
-	FusingStatus                          string            `json:"FusingStatus"`
-	HardwareModel                         string            `json:"HardwareModel"`
-	HardwarePlatform                      string            `json:"HardwarePlatform"`
-	HasSiDP                               string            `json:"HasSiDP"`
-	HostAttached                          string            `json:"HostAttached"`
-	HumanReadableProductVersionString     string            `json:"HumanReadableProductVersionString"`
-	InternationalMobileEquipmentIdentity  string            `json:"InternationalMobileEquipmentIdentity"`
-	InternationalMobileEquipmentIdentity2 string            `json:"InternationalMobileEquipmentIdentity2"`
-	MLBSerialNumber                       string            `json:"MLBSerialNumber"`
-	MobileEquipmentIdentifier             string            `json:"MobileEquipmentIdentifier"`
-	MobileSubscriberCountryCode           string            `json:"MobileSubscriberCountryCode"`
-	MobileSubscriberNetworkCode           string            `json:"MobileSubscriberNetworkCode"`
-	ModelNumber                           string            `json:"ModelNumber"`
-	NonVolatileRAM                        map[string]string `json:"NonVolatileRAM"`
-	PairRecordProtectionClass             string            `json:"PairRecordProtectionClass"`
-	PartitionType                         string            `json:"PartitionType"`
-	PasswordProtected                     string            `json:"PasswordProtected"`
-	PkHash                                string            `json:"PkHash"`
-	ProductName                           string            `json:"ProductName"`
-	ProductType                           string            `json:"ProductType"`
-	ProductVersion                        string            `json:"ProductVersion"`
-	ProductionSOC                         string            `json:"ProductionSOC"`
-	ProtocolVersion                       string            `json:"ProtocolVersion"`
-	ProximitySensorCalibration            string            `json:"ProximitySensorCalibration"`
-	RegionInfo                            string            `json:"RegionInfo"`
-	SIMStatus                             string            `json:"SIMStatus"`
-	SIMTrayStatus                         string            `json:"SIMTrayStatus"`
-	SerialNumber                          string            `json:"SerialNumber"`
-	SoftwareBehavior                      string            `json:"SoftwareBehavior"`
-	SoftwareBundleVersion                 string            `json:"SoftwareBundleVersion"`
-	SupportedDeviceFamilies               []int             `json:"SupportedDeviceFamilies"`
-	TelephonyCapability                   string            `json:"TelephonyCapability"`
-	TimeIntervalSince1970                 string            `json:"TimeIntervalSince1970"`
-	TimeZone                              string            `json:"TimeZone"`
-	TimeZoneOffsetFromUTC                 string            `json:"TimeZoneOffsetFromUTC"`
-	TrustedHostAttached                   string            `json:"TrustedHostAttached"`
-	UniqueChipID                          string            `json:"UniqueChipID"`
-	UniqueDeviceID                        string            `json:"UniqueDeviceID"`
-	UseRaptorCerts                        string            `json:"UseRaptorCerts"`
-	Uses24HourClock                       string            `json:"Uses24HourClock"`
-	WiFiAddress                           string            `json:"WiFiAddress"`
-	WirelessBoardSerialNumber             string            `json:"WirelessBoardSerialNumber"`
-	KCTPostponementStatus                 string            `json:"kCTPostponementStatus"`
-}
-
 type WdaStatus struct {
 	Status    string `json:"status"`
 	SessionId string `json:"sessionId"`
@@ -171,6 +98,7 @@ func SendWSMessageToConn(conn *websocket.Conn, msgType string, data interface{})
 }
 
 func UpdateWdaStatus(udid, status, sessionId, detail string) {
+	log.Info("Start update WDA")
 	stateMu.Lock()
 	state.Wda = WdaStatus{
 		Status:    status,
@@ -179,7 +107,11 @@ func UpdateWdaStatus(udid, status, sessionId, detail string) {
 	}
 	updated := state.Wda
 	stateMu.Unlock()
+	log.Info("Stop update WDA")
+	log.Info("Start send ws WDA")
 	SendWSMessage("wda_status", map[string]interface{}{"udid": udid, "status": updated})
+	log.Info("Stop send ws WDA")
+
 }
 
 //func UpdateDeviceInfo(info DeviceInfo) {
