867a37c1677a228c20394bdca85be16696360d28
rrr <ruslan.ramazanov@antara-tech.ru>
2025-09-06 09:48:04 +0400

    wda & mjpeg port forward

--- run.sh
+++ run.sh
@@ -3,12 +3,20 @@ export USBMUXD_SOCKET_ADDRESS=/var/run/usbmuxd
 
 cd /app || exit
 
+# Функция для корректного завершения usbmuxd
+cleanup() {
+    echo "Stopping usbmuxd..."
+    kill -s SIGTERM $UMUXD_PID
+    wait $UMUXD_PID
+    echo "usbmuxd stopped."
+}
+
+# Обработка сигнала завершения контейнера
+trap 'cleanup' EXIT
+
 # Запускаем usbmuxd в фоне, чтобы иметь возможность запускать peer
 usbmuxd -f &
 UMUXD_PID=$!
 
 # Запускаем основной процесс
 ./peer
-
-# При выходе контейнера убиваем usbmuxd
-kill $UMUXD_PID
