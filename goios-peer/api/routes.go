71c33868159dd815119507758151f2df3c030ea8
rrr <ruslan.ramazanov@antara-tech.ru>
2025-09-18 21:36:52 +0400

    fix pipeline

--- goios-peer/api/routes.go
+++ goios-peer/api/routes.go
@@ -77,7 +77,7 @@ func streamRoutes(group *gin.RouterGroup) {
 	router := group.Group("/stream")
 	router.Use(LimitNumClientsUDID())
 	router.POST("/start", StartStream)
-	router.POST("/start", StartStream2)
+	router.POST("/startparam", StartStream2)
 
 	router.DELETE("/stop", StopStream)
 	router.GET("/status", StatusStream)
